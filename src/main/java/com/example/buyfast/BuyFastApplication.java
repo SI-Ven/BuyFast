b9d9a851523450dbe2e4e8ed82154d506b27533d
Loun Siven <lounseven59@gmail.com>
2025-10-28 10:44:05 +0700

    Initial commit - Spring Boot project

---
+++ src/main/java/com/example/buyfast/BuyFastApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyFastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyFastApplication.class, args);
+    }
+
+}
